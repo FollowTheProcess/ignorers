4fdcbe21804703895197611383c6e47dadc53fde
Tom Fleet <tomfleet2018@gmail.com>
2023-02-05 14:49:55 +0000

    CLI tweaks

--- src/cli.rs
+++ src/cli.rs
@@ -10,11 +10,16 @@ Generate great gitignore files, straight from the command line! 🛠️
 #[command(author, version, about, long_about=LONG_ABOUT)]
 struct Cli {
     /// List of targets to generate a gitignore for
+    #[arg(required = true)]
     targets: Vec<String>,
 
     /// Print the list of available targets
     #[arg(short, long)]
     list: bool,
+
+    /// Print the gitignore to stdout instead of writing to a file
+    #[arg(short, long)]
+    stdout: bool,
 }
 
 /// Parse the CLI arguments and run the program
@@ -28,6 +33,12 @@ pub fn run() {
     }
 
     println!("Generating gitignore for {:?}", cli.targets);
+
+    if cli.stdout {
+        println!("Printing to stdout...");
+    } else {
+        println!("Writing to file...");
+    }
 }
 
 #[cfg(test)]
